fef0c8dfef51059b80e23cb097cc3954b188399b
Anthony Budd <anthonybudd94@gmail.com>
2026-04-15 12:21:03 -0500

    Refactored tests to use TS

--- tests/runners/mocha/init.js
+++
@@ -1,3 +0,0 @@
-require('ts-node').register({
-    project: "tests/runners/mocha/tsconfig.json"
-});
---
+++ tests/runners/mocha/init.ts
@@ -0,0 +1,2 @@
+// Intentionally empty. This file keeps the mocha bootstrap in TypeScript.
+export {};
